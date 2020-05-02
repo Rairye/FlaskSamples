4d21e0860dbb55ce67ebcdbfa13896256ced12ab
Rairye <37942271+Rairye@users.noreply.github.com>
2020-05-03 07:49:45 +0900

    Update app.py

--- TalentDatabase/app.py
+++ TalentDatabase/app.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template, request, jsonify, flash, url_for, session
+from flask import  Flask, render_template, request, jsonify, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
